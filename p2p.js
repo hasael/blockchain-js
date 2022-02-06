548ba2b1e8d3ae45edf8172fc2f6c81954c8a145
Elman Hasa <elman.hasa@tesco.com>
2022-02-06 19:41:28 +0000

    Initial commit

---
+++ p2p.js
@@ -0,0 +1,104 @@
+import getPort from 'get-port';
+import crypto from 'crypto';
+import Swarm from 'discovery-swarm';
+import defaults from 'dat-swarm-defaults';
+
+
+
+const peers = {};
+let connSeq = 0;
+let channel = 'myBlockchain';
+
+const myPeerId = crypto.randomBytes(32);
+console.log('myPeerId: ' + myPeerId.toString('hex'));
+
+const config = defaults({
+    id: myPeerId,
+});
+
+const swarm = Swarm(config);
+
+
+(async () => {
+    const port = await getPort();
+
+    swarm.listen(port);
+    console.log('Listening port: ' + port);
+
+    swarm.join(channel);
+    swarm.on('connection', (conn, info) => {
+        const seq = connSeq;
+        const peerId = info.id.toString('hex');
+        console.log(`Connected #${seq} to peer: ${peerId}`);
+        if (info.initiator) {
+            try {
+                conn.setKeepAlive(true, 600);
+            } catch (exception) {
+                console.log('exception', exception);
+            }
+        }
+
+        conn.on('data', data => {
+            let message = JSON.parse(data);
+            console.log('----------- Received Message start -------------');
+            console.log(
+                'from: ' + peerId.toString('hex'),
+                'to: ' + peerId.toString(message.to),
+                'my: ' + myPeerId.toString('hex'),
+                'type: ' + JSON.stringify(message.type)
+            );
+            console.log('----------- Received Message end -------------');
+
+        });
+
+        conn.on('close', () => {
+            console.log(`Connection ${seq} closed, peerId: ${peerId}`);
+            if (peers[peerId].seq === seq) {
+                delete peers[peerId]
+            }
+        });
+
+        if (!peers[peerId]) {
+            peers[peerId] = {}
+        }
+        peers[peerId].conn = conn;
+        peers[peerId].seq = seq;
+        connSeq++
+    })
+})();
+
+
+setTimeout(function () {
+    writeMessageToPeers('hello', null);
+}, 10000);
+
+function writeMessageToPeers(type, data) {
+    for (let id in peers) {
+        console.log('-------- writeMessageToPeers start -------- ');
+        console.log('type: ' + type + ', to: ' + id);
+        console.log('-------- writeMessageToPeers end ----------- ');
+        sendMessage(id, type, data);
+    }
+};
+
+function writeMessageToPeerToId(toId, type, data){
+    for (let id in peers) {
+        if (id === toId) {
+            console.log('-------- writeMessageToPeerToId start -------- ');
+            console.log('type: ' + type + ', to: ' + toId);
+            console.log('-------- writeMessageToPeerToId end ----------- ');
+            sendMessage(id, type, data);
+        }
+    }
+};
+
+function sendMessage(id, type, data){
+    peers[id].conn.write(JSON.stringify(
+        {
+            to: id,
+            from: myPeerId,
+            type: type,
+            data: data
+        }
+    ));
+};

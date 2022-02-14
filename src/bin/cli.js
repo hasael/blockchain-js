a71f81240923798003038ccf4ade6d0e00185c01
Elman Hasa <elman.hasa@tesco.com>
2022-02-14 22:32:13 +0000

    Add cli

---
+++ src/bin/cli.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var verbose = process.argv.indexOf('--verbose') !== -1;
+var insane = process.argv.indexOf('--insane') !== -1;
+
+if (verbose || insane)
+    process.env.GH_VERBOSE = true;
+
+if (insane)
+    process.env.GH_VERBOSE_INSANE = true;
+
+require('../cli_lib/cmd.js').run();
